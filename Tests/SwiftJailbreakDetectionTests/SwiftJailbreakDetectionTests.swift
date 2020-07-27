b377b77609db1cc22affadfe283cd851f1b9401e
Giuseppe Travasoni <giuseppe.travasoni@gmail.com>
2020-07-27 23:06:34 +0200

    jailbreak base detection

--- Tests/SwiftJailbreakDetectionTests/SwiftJailbreakDetectionTests.swift
+++ Tests/SwiftJailbreakDetectionTests/SwiftJailbreakDetectionTests.swift
@@ -3,10 +3,8 @@ import XCTest
 
 final class SwiftJailbreakDetectionTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SwiftJailbreakDetection().text, "Hello, World!")
+        
+        XCTAssert(JailbreakDetection.isJailbroken)
     }
 
     static var allTests = [
